ea14a7d83d25880ba14c9d6bea6060e4bdb0aba1
KRISHNENDU M R <130251084+Krishnendu72@users.noreply.github.com>
2024-07-21 02:02:27 +0530

    1.7

--- src/Result.jsx
+++ src/Result.jsx
@@ -3,6 +3,19 @@ import React from 'react';
 const Result = ({ result }) => {
     const totalClicks = result.reduce((sum, item) => sum + item.clicks, 0);
 
+    const buttonClicks = result.reduce((acc, item) => {
+        if (item.button === "good") {
+            acc.goodClicks += item.clicks;
+        } else if (item.button === "bad") {
+            acc.badClicks += item.clicks;
+        }
+        return acc;
+    }, { goodClicks: 0, badClicks: 0 });
+
+    const average = (buttonClicks.goodClicks - buttonClicks.badClicks) / totalClicks;
+    const good = buttonClicks.goodClicks / totalClicks;
+
+
     return (
         <div>
             {result.map((item, index) => (
@@ -10,6 +23,9 @@ const Result = ({ result }) => {
                     <p>{item.button}: {item.clicks}</p>
                 </div>
             ))}
+            <p>Total = {totalClicks}</p>
+            <p>Average = {average}</p>
+            <p>Positive = {good} </p>
         </div>
     );
 };
